93830eccd202994750693535cacadcb2f7df0b2d
kapilAtRutty <kapil.rajpopat@rutgers.edu>
2024-12-02 23:02:20 -0500

    Update command.c

--- command.c
+++ command.c
@@ -1,38 +1,39 @@
-#include "command.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/wait.h>
 #include <string.h>
-
-void execute_external_command(char **tokens) {
-    int input_fd = -1, output_fd = -1;
-    handle_redirection(tokens, &input_fd, &output_fd);
-
-    pid_t pid = fork();
-    if (pid < 0) {
-        perror("fork");
-        return;
-    }
-
-    if (pid == 0) {
-        if (input_fd != -1) {
-            dup2(input_fd, STDIN_FILENO);
-            close(input_fd);
-        }
-        if (output_fd != -1) {
-            dup2(output_fd, STDOUT_FILENO);
-            close(output_fd);
-        }
-        execvp(tokens[0], tokens);
-        perror("execvp");
-        exit(EXIT_FAILURE);
-    } else {
-        wait(NULL);
-    }
-}
+#include <fcntl.h>
+#include <unistd.h>
 
 void handle_redirection(char **tokens, int *input_fd, int *output_fd) {
     for (int i = 0; tokens[i] != NULL; i++) {
-        if (strcmp(tokens &#8203;:contentReference[oaicite:0]{index=0}&#8203;
+        if (strcmp(tokens[i], "<") == 0) {
+            if (tokens[i + 1] == NULL) {
+                fprintf(stderr, "Syntax error: no file specified for input redirection\n");
+                return;
+            }
+            *input_fd = open(tokens[i + 1], O_RDONLY);
+            if (*input_fd < 0) {
+                perror("open input file");
+                return;
+            }
+            free(tokens[i]);
+            free(tokens[i + 1]);
+            tokens[i] = NULL;
+            tokens[i + 1] = NULL;
+        } else if (strcmp(tokens[i], ">") == 0) {
+            if (tokens[i + 1] == NULL) {
+                fprintf(stderr, "Syntax error: no file specified for output redirection\n");
+                return;
+            }
+            *output_fd = open(tokens[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (*output_fd < 0) {
+                perror("open output file");
+                return;
+            }
+            free(tokens[i]);
+            free(tokens[i + 1]);
+            tokens[i] = NULL;
+            tokens[i + 1] = NULL;
+        }
+    }
+}
